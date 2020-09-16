61abb9c45fe9a334820f0f694ef9d44701172a74
Marek Sontag <marek.sontag@infobip.com>
2020-09-16 14:31:53 +0200

    NamedPropertyTypeProvider added together with handling in PresentPropertyJsonTypeResolver

--- infobip-jackson-extension-api/src/main/java/com/infobip/jackson/PresentPropertyJsonTypeResolver.java
+++ infobip-jackson-extension-api/src/main/java/com/infobip/jackson/PresentPropertyJsonTypeResolver.java
@@ -1,7 +1,7 @@
 package com.infobip.jackson;
 
-import java.util.Map;
-import java.util.Objects;
+import java.lang.reflect.Type;
+import java.util.*;
 
 public class PresentPropertyJsonTypeResolver<E extends Enum<E> & TypeProvider> implements JsonTypeResolver {
 
@@ -14,11 +14,33 @@ public class PresentPropertyJsonTypeResolver<E extends Enum<E> & TypeProvider> i
     @Override
     public final Class<?> resolve(Map<String, Object> json) {
         for (E constant : type.getEnumConstants()) {
-            if (json.containsKey(constant.toString().toLowerCase())) {
+            if (json.containsKey(resolvePropertyName(constant))) {
                 return constant.getType();
             }
         }
 
         throw new IllegalArgumentException("Failed to resolve type " + json);
+    }
+
+    private String resolvePropertyName(E constant) {
+        for (Type type : getAllInterfaces(constant.getClass())) {
+            if (type.equals(NamedPropertyTypeProvider.class)) {
+                return ((NamedPropertyTypeProvider) constant).getJsonPropertyName();
+            }
+        }
+        return constant.toString().toLowerCase();
+    }
+
+    private List<Type> getAllInterfaces(Class<?> type) {
+        List<Type> allInterfaces = new ArrayList<Type>();
+
+        for (Type anInterface : type.getGenericInterfaces()) {
+            allInterfaces.add(anInterface);
+            if (anInterface instanceof Class<?>) {
+                allInterfaces.addAll(getAllInterfaces((Class<?>) anInterface));
+            }
+        }
+
+        return allInterfaces;
     }
 }
