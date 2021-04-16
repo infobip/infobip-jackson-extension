c3b253bb885f39c6489ba3943ca937d525d2ca07
lpandzic <lovro.pandzic@infobip.com>
2021-04-16 08:25:20 +0200

    added SingleArgumentPropertiesCreatorModeAnnotationIntrospectorTest upgraded spring dependencies

--- infobip-jackson-extension-module/src/main/java/com/infobip/jackson/SingleArgumentPropertiesCreatorModeAnnotationIntrospector.java
+++ infobip-jackson-extension-module/src/main/java/com/infobip/jackson/SingleArgumentPropertiesCreatorModeAnnotationIntrospector.java
@@ -5,32 +5,23 @@ import com.fasterxml.jackson.databind.cfg.MapperConfig;
 import com.fasterxml.jackson.databind.introspect.*;
 
 import java.lang.reflect.Field;
-import java.util.Objects;
 import java.util.stream.Stream;
 
 public class SingleArgumentPropertiesCreatorModeAnnotationIntrospector extends NopAnnotationIntrospector {
 
     @Override
     public JsonCreator.Mode findCreatorAnnotation(MapperConfig<?> config, Annotated a) {
-        JsonCreator ann = _findAnnotation(a, JsonCreator.class);
-
-        if (Objects.nonNull(ann)) {
-            return ann.mode();
-        }
-
         if (!(a instanceof AnnotatedConstructor)) {
             return null;
         }
 
         AnnotatedConstructor annotatedConstructor = (AnnotatedConstructor) a;
-        Class<?> declaringClass = annotatedConstructor.getDeclaringClass();
-        if (Throwable.class.isAssignableFrom(declaringClass)) {
-            return null;
-        }
 
         if (annotatedConstructor.getParameterCount() != 1) {
             return null;
         }
+
+        Class<?> declaringClass = annotatedConstructor.getDeclaringClass();
 
         if (declaringClass.getDeclaredConstructors().length > 1) {
             return null;
---
+++ infobip-jackson-extension-module/src/test/java/com/infobip/jackson/SingleArgumentPropertiesCreatorModeAnnotationIntrospectorTest.java
@@ -0,0 +1,139 @@
+package com.infobip.jackson;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+import lombok.*;
+import org.assertj.core.api.BDDAssertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+class SingleArgumentPropertiesCreatorModeAnnotationIntrospectorTest extends TestBase {
+
+    @Test
+    void shouldDeserializeClassWithSingleFieldAndOnlySingleParameterConstructor() throws JsonProcessingException {
+        // given
+        String givenJson = "{'foo':'givenFoo'}";
+
+        // when
+        ClassWithSingleFieldAndOnlySingleParameterConstructor actual = objectMapper.readValue(givenJson,
+                                                                                              ClassWithSingleFieldAndOnlySingleParameterConstructor.class);
+
+        // then
+        then(actual).isEqualTo(new ClassWithSingleFieldAndOnlySingleParameterConstructor("givenFoo"));
+    }
+
+    @Test
+    void shouldDeserializeClassWithMultipleFieldsAndOnlySingleParameterConstructor() throws
+            JsonProcessingException {
+        // given
+        String givenJson = "{'foo':'givenFoo'}";
+
+        // when
+        ClassWithMultipleFieldsAndOnlySingleParameterConstructor actual = objectMapper.readValue(givenJson,
+                                                                                                 ClassWithMultipleFieldsAndOnlySingleParameterConstructor.class);
+
+        // then
+        then(actual).isEqualTo(new ClassWithMultipleFieldsAndOnlySingleParameterConstructor("givenFoo"));
+    }
+
+    @Test
+    void shouldDeserializeClassWithDelegatingCreatorConstructor() throws
+            JsonProcessingException {
+        // given
+        String givenJson = "{'foo':'givenFoo'}";
+
+        // when
+        ClassWithMultipleFieldsAndOnlySingleParameterConstructor actual = objectMapper.readValue(givenJson,
+                                                                                                 ClassWithMultipleFieldsAndOnlySingleParameterConstructor.class);
+
+        // then
+        then(actual).isEqualTo(new ClassWithMultipleFieldsAndOnlySingleParameterConstructor("givenFoo"));
+    }
+
+    @Test
+    void shouldFailToDeserializeClassWithMultipleConstructors() {
+        // given
+        String givenJson = "{'foo':'givenFoo'}";
+
+        // when
+        Throwable actual = BDDAssertions.catchThrowable(
+                () -> objectMapper.readValue(givenJson, ClassWithMultipleConstructors.class));
+
+        // then
+        then(actual).isInstanceOf(MismatchedInputException.class)
+                    .hasMessage("Cannot construct instance of `com.infobip.jackson.SingleArgumentPropertiesCreatorModeAnnotationIntrospectorTest$ClassWithMultipleConstructors` (although at least one Creator exists): cannot deserialize from Object value (no delegate- or property-based Creator)\n" +
+                                        " at [Source: (String)\"{'foo':'givenFoo'}\"; line: 1, column: 2]");
+    }
+
+    @Test
+    void shouldFailToDeserializeClassWithMismatchingParameterName() {
+        // given
+        String givenJson = "{'foo':'givenFooBar'}";
+
+        // when
+        Throwable actual = BDDAssertions.catchThrowable(() -> objectMapper.readValue(givenJson,
+                                                                                     ClassWithMismatchingParameterName.class));
+
+        // then
+        then(actual).isInstanceOf(MismatchedInputException.class)
+                    .hasMessage(
+                            "Cannot construct instance of `com.infobip.jackson.SingleArgumentPropertiesCreatorModeAnnotationIntrospectorTest$ClassWithMismatchingParameterName` (although at least one Creator exists): cannot deserialize from Object value (no delegate- or property-based Creator)\n" +
+                                    " at [Source: (String)\"{'foo':'givenFooBar'}\"; line: 1, column: 2]");
+    }
+
+    @Value
+    static class ClassWithSingleFieldAndOnlySingleParameterConstructor {
+
+        private final String foo;
+
+        public ClassWithSingleFieldAndOnlySingleParameterConstructor(String foo) {
+            this.foo = foo;
+        }
+    }
+
+    @Value
+    static class ClassWithMultipleFieldsAndOnlySingleParameterConstructor {
+
+        private final String foo;
+        private final String bar = "bar";
+    }
+
+    @Value
+    static class ClassWithDelegatingCreatorConstructor {
+
+        private final String foo;
+
+        @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+        public ClassWithDelegatingCreatorConstructor(Map<String, String> foo) {
+            this.foo = foo.get("foo");
+        }
+    }
+
+    @ToString
+    @EqualsAndHashCode
+    static class ClassWithMultipleConstructors {
+
+        private final String foo;
+
+        public ClassWithMultipleConstructors(String foo) {
+            this.foo = foo;
+        }
+
+        public ClassWithMultipleConstructors(Integer bar) {
+            this.foo = null;
+        }
+    }
+
+    static class ClassWithMismatchingParameterName {
+
+        private final String foo;
+
+        public ClassWithMismatchingParameterName(String bar) {
+            this.foo = bar;
+        }
+    }
+}
