0a96d7dbbb369de49ff3d75835a58df30979d680
lpandzic <lovro.pandzic@infobip.com>
2026-04-23 13:13:19 +0200

    LegacyJacksonAutoConfiguration fix

--- infobip-jackson-extension-spring-boot-starter/src/main/java/com/infobip/jackson/LegacyJacksonAutoConfiguration.java
+++ infobip-jackson-extension-spring-boot-starter/src/main/java/com/infobip/jackson/LegacyJacksonAutoConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
-@ConditionalOnClass(ObjectMapper)
+@ConditionalOnClass(ObjectMapper.class)
 @Deprecated(forRemoval = true)
 @Configuration
 public class LegacyJacksonAutoConfiguration {
